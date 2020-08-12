d9544f3e8d313931bbf21a93e4ae90b6bd870974
Carrie Mathieu <carrieburks@gmail.com>
2020-08-12 21:19:24 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -7,7 +7,7 @@ class School
   end
   
   def add_student(name, grade)
-    if (roster.include?(grade))
+    if !(roster.include?(grade))
       roster[grade] = []
     end
     roster[grade] << name
